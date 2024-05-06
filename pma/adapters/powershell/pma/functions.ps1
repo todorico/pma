f2c69ed8fa91b38dc84e2c72e959b4138eea385a
Thibault ODORICO <odoricothibault@gmail.com>
2024-05-06 14:45:00 +0200

    add pma adapter

---
+++ pma/adapters/powershell/pma/functions.ps1
@@ -0,0 +1,103 @@
+$PMA_HOME = $PMA_HOME, $env:PMA_HOME, ([System.IO.DirectoryInfo] $PSScriptRoot).Parent.Parent.Parent | Select-Object -First 1
+
+. "$PMA_HOME/tools/utilities.ps1"
+
+#
+# List
+#
+
+function list_packages {
+    return list_preset_files -filter "*.ps1" | Select-Object -ExpandProperty BaseName
+}
+
+#
+# Install
+#
+
+function install_adapter {
+
+}
+
+function install_packages {
+    param([Parameter(Mandatory=$false)] [string[]] $packages)
+
+    if ($packages -contains "all") {
+        $packages = list_packages
+    }
+
+    Set-Alias -Name pma -Value invoke_pma -Option AllScope
+
+    foreach ($package in $packages) {
+        $presetScript = list_preset_files "$package.ps1"
+
+        if(-not $presetScript) {
+            Write-Host "Error: No preset package named: $package" -ForegroundColor Red
+            exit 1
+        }
+
+        & $presetScript.FullName
+
+        # try { RETURN & $scriptBlock }
+        # Catch { RETURN }
+        # Finally { $ErrorActionPreference = $OldPreference }
+        # invoke-expression -Command "$($presetScript.FullName)"
+        # & $presetScript.FullName
+
+        # $LastExitCode
+        # Get-Content  | Invoke-Expression
+    }
+}
+
+#
+# Update
+#
+
+function update_adapter {
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+    Invoke-RestMethod -Uri https://raw.githubusercontent.com/todorico/pma/main/install.ps1 | Invoke-Expression
+}
+
+function update_packages {
+    param([Parameter(Mandatory=$false)] [string[]] $packages = @())
+}
+
+#
+# Uninstall
+#
+
+function uninstall_adapter {
+
+}
+
+function uninstall_packages {
+
+}
+
+#
+# Utils
+#
+
+function invoke_pma {
+    $script = "$PMA_HOME/scripts/pma.ps1"
+    & $script @args
+}
+
+function is_adapter_installed {
+    return $true
+}
+
+function check_adapter_installed {
+    param ([Parameter(Mandatory=$true)] [string] $adapter)
+
+    if (is_adapter_installed) {
+        return
+    }
+
+    Write-Output @"
+Error: Adapter '$adapter' is not installed. Use the following command to install it.
+
+  pma install $adapter
+"@
+
+    exit 1
+}
---
+++ pma/adapters/powershell/pma/list.ps1
@@ -0,0 +1,31 @@
+param(
+    [Parameter(Mandatory=$true)] [string] $COMMAND,
+    [Parameter(Mandatory=$true)] [string] $ADAPTER,
+    [Parameter(ValueFromRemainingArguments=$true)] [string[]] $args
+)
+
+function usage {
+    Write-Output @"
+Usage: pma $COMMAND $ADAPTER [-packages]
+
+Options:
+  -packages                 List installed packages.
+"@
+}
+
+
+. "$PSScriptRoot/functions.ps1"
+
+function main {
+    # check_adapter_installed $ADAPTER
+
+    if (-not $args -or ($args -eq @("-packages"))) {
+        list_packages
+    }
+    else {
+        usage
+        exit 1
+    }
+}
+
+main @args
